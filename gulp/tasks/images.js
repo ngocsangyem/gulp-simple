08d2302b7cafeae743ca55be3158895520f235a2
ngocsangyem <nnsang24@gmail.com>
2019-11-20 22:00:15 +0700

    fix: remove imagemin svg

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,11 +1,11 @@
-import gulp from 'gulp';
+import gulp from "gulp";
 
-import { plugins, args, config, taskTarget, browserSync } from '../utils';
+import { plugins, args, config, taskTarget, browserSync } from "../utils";
 
 const dirs = config.directories;
 const dest = `${taskTarget}/${dirs.images}`;
 
-gulp.task('images', () => {
+gulp.task("images", () => {
 	return gulp
 		.src(
 			`${dirs.source}${dirs.assets}${dirs.images}**/*.{jpg,jpeg,gif,svg,png}`
@@ -14,12 +14,15 @@ gulp.task('images', () => {
 		.pipe(
 			plugins.if(
 				args.production,
-				plugins.imagemin([
-					plugins.imagemin.jpegtran({ progressive: true }),
-					plugins.imagemin.svgo({
-						plugins: [{ removeViewBox: false }]
-					})
-				])
+				plugins.imagemin(
+					[plugins.imagemin.jpegtran({ progressive: true })],
+					{
+						verbose: true,
+						interlaced: true,
+						progressive: true,
+						optimizationLevel: 5
+					}
+				)
 			)
 		)
 		.pipe(gulp.dest(dest));
